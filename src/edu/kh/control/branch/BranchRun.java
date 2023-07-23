a0b1ff4a315d727e90a807ebccad443840c00547
taebeom <ptbyahoo12@gmail.com>
2023-07-24 08:20:15 +0900

    commit

--- src/edu/kh/control/branch/BranchRun.java
+++ src/edu/kh/control/branch/BranchRun.java
@@ -4,12 +4,12 @@ public class BranchRun {
 	public static void main(String[] args) {
 		BranchExample branchEx = new BranchExample();
 		//branchEx.ex1();
-		//branchEx.ex2();
+		branchEx.ex2();
 		//branchEx.ex3();
 		//branchEx.ex4();
 		//branchEx.ex5();
 		//branchEx.ex6();
-		branchEx.ex7();
+		//branchEx.ex7();
 		
 	}
 }
--- src/edu/kh/control/practice/Run2.java
+++ src/edu/kh/control/practice/Run2.java
@@ -3,19 +3,19 @@ package edu.kh.control.practice;
 public class Run2 {
 
 	public static void main(String[] args) {
-		LoopPractice sr = new LoopPractice();
+		LoopPractice2 sr = new LoopPractice2();
 		//sr.practice1();
 		//sr.practice2();
 		//sr.practice3();
 		//sr.practice4();
 		//sr.practice5();
 		
-		//오늘
+		
 		//sr.practice6();
 		//sr.practice7();
 		//sr.practice8();
 		//sr.practice9();
-		sr.practice10();
+		//sr.practice10();
 	}
 
 }
